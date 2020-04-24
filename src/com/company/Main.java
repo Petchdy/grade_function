2f6b0af885463cb48bea3bd8b87f4708492e1e26
Petchdy <skulwisit@gmail.com>
2020-04-24 13:47:09 +0700

    2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,33 +5,35 @@ import com.sun.scenario.effect.impl.sw.java.JSWColorAdjustPeer;
 import java.util.Scanner;
 
 public class Main {
-    static double grade (int score){
+    static void grade(int score){
         if (score>=80){
-            return 4;
+            System.out.printf("4");
         }
         else if (score>=75){
-            return 3.5;
+            System.out.printf("3.5");
         }
         else if (score>=70){
-            return 3;
+            System.out.printf("3");
         }
         else if (score>=65){
-            return 2.5;
+            System.out.printf("2.5");
         }
         else if (score>=60){
-            return 2;
+            System.out.printf("2");
         }
         else if (score>=55){
-            return 1.5;
+            System.out.printf("1.5");
         }
         else if (score>=50){
-            return 1;
+            System.out.printf("1");
         }
-        return 0;
+        else {
+            System.out.printf("0");
+        }
     }
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.println(grade(scan.nextInt()));
+        grade(scan.nextInt());
     }
 }
